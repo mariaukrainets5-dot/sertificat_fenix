90df7e3be87ec35e94a43fd985fdc1ce2a1411c9
mariaukrainets5-dot <mariaukrainets5@gmail.com>
2026-02-18 21:19:05 +0200

    Rename company to FENIX ARMY STORE

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 import { PresetAmount } from "./types";
 
 export const PRESET_AMOUNTS: PresetAmount[] = [500, 1000, 2000, 5000];
-export const COMPANY_NAME = "FENIX VOENTORG";
+export const COMPANY_NAME = "FENIX ARMY STORE";
 export const WEBSITE_URL = "fenix-voentorg.com.ua";
 export const DEFAULT_MANAGER = "Менеджер"; // Default placeholder
